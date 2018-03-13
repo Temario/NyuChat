d92c175b3a5157b4eaf4f225071a42a7df0465f8
Marcosalco <markosgolfo@gmail.com>
2018-03-13 18:05:04 +0100

    Add files via upload
    Todo validado,  creación de ficheros con todos los mensajes validados

---
+++ cliente.py
@@ -0,0 +1,89 @@
+"""
+Sencillo chat de terminal multiplataforma pensado para redes locales.
+Escrito en Python 3.6.4
+"""
+
+import sys
+import time
+import socket
+import threading
+
+# Variable de salida.
+exit = False
+
+# Mensaje de ayuda cuando no se proporcionan dos argumentos.
+def helpmsg(str):
+    print(str)
+    sys.exit()
+
+if len(sys.argv) != 3:
+    helpmsg("Ayuda: "+sys.argv[0]+" IP puerto")
+
+# Valida los parámetros y los pasa a variables.
+try:
+    socket.inet_pton(socket.AF_INET, sys.argv[1])
+except(OSError):
+    helpmsg("Error: dirección IP inválida.")
+
+ip = sys.argv[1]
+
+try:
+    port = int(sys.argv[2])
+except(ValueError):
+    helpmsg("Error: El puerto debe ser un valor numérico entero.")
+
+if port < 0 or port > 65535:
+    helpmsg("Error: Puerto fuera del rango válido [0-65535].")
+
+# Pide el nombre de usuario.
+valid = False
+while not valid:
+    user = input("Nombre: ")
+    if len(user) > 0 and len(user) <= 20:
+        valid = True
+
+# Función de salida.
+def stopandquit():
+    exit = True
+    server.close()
+    sys.exit()
+
+# Hilo de escucha del servidor.
+def serverthread():
+    while not exit:
+        try:
+            msg = server.recv(2048).decode("utf-8")
+            if msg == "!q":
+                print(time.strftime("%H:%M:%S")+" - El servidor se ha desconectado -")
+                stopandquit()
+            else:
+                print(msg)
+        except:
+            break
+
+# Crea el socket TCP servidor e intenta conectar con el mismo.
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    server.connect((ip, port))
+except(ConnectionRefusedError):
+    helpmsg("Error: El servidor ha rechazado la conexión.")
+
+# Empieza el hilo de escucha del servidor.
+threading.Thread(target=serverthread).start()
+
+# Ayuda al inicio.
+print("!q : salir")
+
+# Bucle principal. Espera entrada y envía mensajes al servidor.
+while not exit:
+    try:
+        msg = input()
+        if msg == "!q":
+            server.send(msg.encode("utf-8"))
+            exit = True
+        else:
+            server.send(("["+user+"]: "+msg).encode("utf-8"))
+    except: #(KeyboardInterrupt, SystemExit):
+        stopandquit()
+
+stopandquit()
---
+++ servere.py
@@ -0,0 +1,135 @@
+"""
+Sencillo chat de terminal multiplataforma pensado para redes locales.
+Escrito en Python 3.6.4
+"""
+
+import sys
+import time
+import socket
+import threading
+
+# Mensaje de ayuda cuando no se proporcionan dos argumentos.
+def helpmsg(str):
+    print(str)
+    sys.exit()
+
+if len(sys.argv) != 3:
+    helpmsg("Ayuda: "+sys.argv[0]+" IP puerto")
+
+# Valida los parámetros y los pasa a variables.
+try:
+    socket.inet_pton(socket.AF_INET, sys.argv[1])
+except(OSError):
+    helpmsg("Error: dirección IP inválida.")
+
+ip = str(sys.argv[1])
+
+try:
+    port = int(sys.argv[2])
+except(ValueError):
+    helpmsg("Error: El puerto debe ser un valor numérico entero.")
+
+if port < 0 or port > 65535:
+    helpmsg("Error: Puerto fuera del rango válido [0-65535].")
+
+# Crea el socket TCP servidor y establece los parámetros.
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Escucha del socket servidor en la IP y el puerto.
+try:
+    server.bind((ip, port))
+except(OSError):
+    helpmsg("Error: Puerto ya en uso.")
+except(socket.gaierror):
+    helpmsg("Error: No se ha podido crear el socket en "+ip+":"+port+".")
+
+server.listen(100) # Hasta 100 clientes simultáneos. Editar si es necesario.
+
+# Array de sockets de clientes y variable de salida.
+clients = []
+exit = False
+
+# Función de eliminación de cliente del array.
+def remove(conn):
+    if conn in clients:
+        clients.remove(conn)
+
+# Función de envío de mensajes a todos los clientes.
+def broadcast(msg, conn):
+    for c in clients:
+        #if c != conn: # Descomentar para no enviar los mensajes de vuelta al emisor.
+        try:
+            c.send(msg.encode("utf-8"))
+        except:
+            c.close()
+            remove(c)
+            
+def fichero(texto):
+    ano=time.strftime("%y-%m-%d")
+    reg=open(ano+"-log.txt","a")
+    reg.write(' - '+texto+'\n')
+    reg.close()
+
+# Hilos de clientes.
+def clientthread(conn, addr):
+    conn.send((time.strftime("%H:%M:%S")+" - Conectado a ["+ip+"] -").encode("utf-8"))
+    while not exit:
+        try:
+            msg = conn.recv(2048).decode("utf-8")
+            if msg == "!q":
+                msg_send = " - ["+addr[0]+"] se ha desconectado -"
+            else:
+                msg_send = " ["+addr[0]+"] "+msg
+            msg_send = time.strftime("%H:%M:%S")+msg_send
+            print(msg_send)
+            fichero(msg_send)
+            broadcast(msg_send, conn)
+        except:
+            remove(conn)
+            break
+
+# Hilo del servidor.
+def serverthread():
+    while not exit:
+        try:
+            conn, addr = server.accept()
+            clients.append(conn)
+            msg_send = time.strftime("%H:%M:%S")+" - ["+addr[0]+"] se ha conectado -"
+            print()
+            fichero(msg_send)
+            threading.Thread(target=clientthread, args=(conn, addr)).start()
+        except:
+            break
+
+# Empieza el hilo servidor.
+threading.Thread(target=serverthread).start()
+
+# Ayuda al inicio.
+msg_send = time.strftime("%H:%M:%S")+" Servidor Iniciado"
+print("Servidor en escucha, !q : salir")
+fichero(msg_send)
+
+# Bucle principal. Espera entrada y envía mensajes del servidor.
+while not exit:
+    try:
+        msg = input()
+        if msg == "!q":
+            msg_send = time.strftime("%H:%M:%S")+" - Servidor cerrado"
+            fichero(msg_send)
+            broadcast("!q", None)
+            exit = True
+            for c in clients:
+                c.close()
+                clients.remove(c)
+            server.close()
+        else:
+            msg_send =time.strftime("%H:%M:%S")+" [Servidor]: "+msg
+            fichero(msg_send)
+            broadcast(msg_send, None)
+    except: #(KeyboardInterrupt, SystemExit):
+        exit = True
+        msg_send = time.strftime("%H:%M:%S")+"Servidor cerrado por excepcion"
+        fichero(msg_send)
+        server.close()
+        sys.exit()
